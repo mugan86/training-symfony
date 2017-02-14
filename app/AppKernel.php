ab247b2a0ea0dd439eedbb566bb22093b8f36d4a
anartzmugika <mugan86@gmail.com>
2017-02-14 18:39:09 +0100

    Remove AppBunble and add CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,8 +20,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Acme\HelloBundle\AcmeHelloBundle(),
+            new Blog\CoreBundle\CoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
